07e3559c9ec332cee46a6ade5c1b9188ce8095f4
Amitai Schlair <schmonz-web-git@schmonz.com>
2016-07-23 19:35:11 -0400

    Inline some graph_create() params in test.

--- check_mandelbrot.c
+++ check_mandelbrot.c
@@ -31,10 +31,8 @@ assert_complex_points_equal(complex double expected, complex double actual)
 START_TEST(test_complex_plane_coordinates)
 {
     const int width = 800, height = width;
-    const double range = 4.0;
-    complex double center = 0 + I * 0;
 
-    graph_t graph = graph_create("dummy-backend", width, height, center, range);
+    graph_t graph = graph_create("", width, height, 0 + I * 0, 4.0);
 
     assert_complex_points_equal(-2 - I * 2, graph.extremes.lower_left);
     assert_complex_points_equal( 2 + I * 2, graph.extremes.upper_right);
@@ -51,10 +49,8 @@ END_TEST
 START_TEST(test_aspect_ratio_in_portrait)
 {
     const int width = 500, height = 800;
-    const double range = 2.0;
-    complex double center = 0.25 + I * -0.3;
 
-    graph_t graph = graph_create("dummy-backend", width, height, center, range);
+    graph_t graph = graph_create("", width, height, 0.25 + I * -0.3, 2.0);
 
     assert_complex_points_equal(-0.375 - I * 1.3, graph.extremes.lower_left);
     assert_complex_points_equal( 0.875 + I * 0.7, graph.extremes.upper_right);
@@ -66,10 +62,8 @@ END_TEST
 START_TEST(test_aspect_ratio_in_landscape)
 {
     const int width = 600, height = 450;
-    const double range = 4.0;
-    complex double center = -0.25 + I * 0.3;
 
-    graph_t graph = graph_create("dummy-backend", width, height, center, range);
+    graph_t graph = graph_create("", width, height, -0.25 + I * 0.3, 4.0);
 
     assert_complex_points_equal(-2.25 - I * 1.2, graph.extremes.lower_left);
     assert_complex_points_equal( 1.75 + I * 1.8, graph.extremes.upper_right);
@@ -83,6 +77,7 @@ START_TEST(test_some_known_points)
     size_t iterations = 2;
     complex double is_in_set = 0.0 + I * 0.0;
     complex double is_not_in_set = 1.5 + I * 1.5;
+
     ck_assert_int_eq(0, choose_escape_color(is_in_set, iterations));
     ck_assert_int_ne(0, choose_escape_color(is_not_in_set, iterations));
 }
